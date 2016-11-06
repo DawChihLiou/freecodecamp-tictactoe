a3d63638fc64ad38f1199c00867500de8bf2b6c1
Daw-Chih Liou <dawochih.liou@gmail.com>
2016-11-06 17:12:12 +0800

    modify actinos

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,37 +1,47 @@
 /*
   state = {
-    won: number, (-1, 0, 1)
+    won: number, (null, -1, 0, 1)
     playas: string, ('x'/'o')
-    playing: boolean,
-    turn: 'x',
     computer: [],
     player: [],
+    winCombos: [
+     [1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9],
+     [1, 4, 7],
+     [2, 5, 8],
+     [3, 6, 9],
+     [1, 5, 9],
+     [7, 5, 3]
+   ]
   }
  */
 
- /*
-   winCombos: [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [7, 5, 3]
-  ],
- */
-
-export const RESTART_GAME = 'RESTART_GAME'
-export const ADD_PLAYER_MOVE = 'ADD_PLAYER_MOVE'
-export const ADD_COMPUTER_MOVE = 'ADD_COMPUTER_MOVE'
-export const SET_PLAYAS = 'SET_PLAYAS'
+export const RESTART_GAME       = 'RESTART_GAME'
+export const ADD_PLAYER_MOVE    = 'ADD_PLAYER_MOVE'
+export const ADD_COMPUTER_MOVE  = 'ADD_COMPUTER_MOVE'
+export const SET_PLAYAS         = 'SET_PLAYAS'
 export const SET_WINNING_STATUS = 'SET_WINNING_STATUS'
 
 export const restartGame = () => {
+  let won       = null
+  let playas    = null
+  let computer  = []
+  let player    = []
+  let winCombos = [
+     [1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9],
+     [1, 4, 7],
+     [2, 5, 8],
+     [3, 6, 9],
+     [1, 5, 9],
+     [7, 5, 3]
+   ]
+
   return {
     type: RESTART_GAME,
-    playing: false
+    defaultState: { won, playas, computer, player, winCombos }
   }
 }
 
@@ -56,7 +66,7 @@ export const playAs = playas => {
   }
 }
 
-expoert const setWinningStatus = status => {
+export const setWinningStatus = status => {
   return {
     type: SET_WINNING_STATUS,
     winningStatus: status
